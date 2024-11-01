5b6300c4f02e9a7bf3bc519127429e8617d56722
The Meowy <meowmeowingstorage00@gmail.com>
2024-11-01 15:05:29 +0300

    golf thing 2nd comm
    Level & Player Controller

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Golf
+{
+    public class LevelController : MonoBehaviour
+    {
+        public StoneSpawner stoneSpawner;
+        private float m_timer;
+        [SerializeField]
+        private float m_delay = 2f;
+
+        public void OnEnable()
+        {
+            m_timer = Time.time - m_delay;
+        }
+
+        private void Update()
+        {
+            if (Time.time > m_timer + m_delay)
+            {
+                stoneSpawner.Spawn();
+                m_timer = Time.time;
+            }
+
+        }
+    }
+}
